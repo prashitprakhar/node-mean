6a633ab0517637722442b6495a6e7fd090d72662
prashitprakhar <prashitprakhar@yahoo.com>
2018-06-03 11:20:59 +0530

    node_ud_full

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+let app = express();
+
+hbs.registerPartials(__dirname + '/public/partials')
+
+
+//registerHelper is used after app.set();
+
+// hbs.registerHelper('uppercase', (text) => {
+//     return text.toUpperCase();
+// })
+
+app.use((req, res, next) => {
+
+    let timestamp = new Date().toString();
+    let log = `${timestamp} - ${req.method} - ${req.url}`
+    console.log(log);
+    fs.appendFile('server.log', log+'\n', (err) => {
+        console.log("Error encountered while logging")
+    })
+    next();
+});
+
+app.use((req, res, next) => {
+    let timestamp = new Date().toString();
+    let log = `${timestamp} - ${req.method} - ${req.url}`
+    console.log(log);
+    fs.appendFile('maintenance.log', log+'\n', (err) => {
+        console.log("Error encountered while logging")
+    })
+    res.render('maintenance.hbs');
+    //next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.send("Hello APIS");
+});
+
+app.get('/about', (req,res) => {
+    res.render('about.hbs', {
+        pageName : 'About Page',
+        currentYear : new Date().getFullYear()
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Server is listening");
+});
